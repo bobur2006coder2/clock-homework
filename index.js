1bcb364934c114f16a2bc140d981f75355c5786a
bobur2006coder <boburdilmurodov702@gmail.com>
2023-06-24 07:54:44 +0500

    f9co

--- index.js
+++ index.js
@@ -19,3 +19,41 @@ function setRotation(element, rotationRatio) {
 }
 
 setClock()
+
+
+function darkMode() {
+  let min=document.querySelector(".minute")
+  let hour=document.querySelector(".hour")
+  let body=document.querySelector("body")
+  let btn=document.querySelector(".dark")
+  let btntwo=document.querySelector(".white")
+  if(btn.classList.contains("none")){
+    btn.classList.remove("none")
+    hour.style.background="black"
+    min.style.background="black"
+    
+  }else{
+    btn.classList.add("none")
+    hour.style.background="white"
+    min.style.background="white"
+  }
+  
+  botomHour.classList.toggle("col")
+  body.classList.toggle("bg-body")
+}
+
+
+
+let botomHour=document.querySelector('.container-for')
+setInterval(()=>{
+  let weekdays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+  const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  let ho=new Date()
+  let sd=ho.getHours()<10?"0"+ho.getHours():ho.getHours();
+  let da=ho.getDay()
+  let hs=ho.getMonth()
+  let se=ho.getSeconds()<10?"0"+ho.getSeconds():ho.getSeconds();
+  let forMin=ho.getMinutes()<10?"0"+ho.getMinutes():ho.getMinutes();
+  botomHour.innerHTML=sd+": "+forMin+" : "+se
+  botomHour.nextElementSibling.innerHTML= ` ${weekdays[da-1]}, ${months[hs]}, ${ho.getUTCDate()}`
+},1000)
